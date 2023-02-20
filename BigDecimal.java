f19a03a3109148599e4ad7f665a2452c8614551d
Himanshu Jha <90416078+HIMANSHU-1504@users.noreply.github.com>
2023-02-20 23:39:26 +0530

    Add files via upload

---
+++ BigDecimal.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+    String []s=new String[n+2];
+    for(int i=0;i<n;i++)
+    {
+        s[i]=sc.next();
+    }
+    for(int i=0;i<n;i++)
+        {
+            BigDecimal max=new BigDecimal(s[i]);
+            int idx=i;
+            for(int j=i+1;j<n;j++)
+            {
+                BigDecimal curr=new BigDecimal(s[j]);
+                if(curr.compareTo(max)==1)
+                {
+                    max=curr;
+                    idx=j;
+                }
+            }
+            String temp=s[i];
+            s[i]=s[idx];
+            s[idx]=temp;
+        }
+    //Output
+    for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+}
